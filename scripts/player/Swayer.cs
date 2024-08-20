b409334441fd30f2251143b39748969824b1da04
Fripe070 <72686066+Fripe070@users.noreply.github.com>
2024-08-20 15:29:48 +0200

    Camera tilt

--- scripts/player/Swayer.cs
+++ scripts/player/Swayer.cs
@@ -6,17 +6,49 @@ namespace Kinematics.scripts.player;
 public partial class Swayer : Node3D
 {
 	[Export] private Player _player;
-	[Export] private float _amplitude = 0.05f;
-	[Export] private float _frequency = 2.4f;
+	[Export] private float _stepAmplitude = 0.05f;
+	[Export] private float _stepFrequency = 4f;
+	[Export] private float _tiltDegPerMPerS = 5f / 15f;
+	[Export] private float _tiltMaxDeg = 15f;
 	
 	[Signal] public delegate void DownStepEventHandler();
 
-	private float _offset = 0;
-	private bool _oldCheck = false;
+	private float _offset;
+	private bool _oldCheck;
 
 	public override void _Process(double delta)
 	{
+		Sway(delta);
+		ViewBob(delta);
+	}
+
+	private void Sway(double delta)
+	{
+		var flatVel = _player.Velocity.WithY(0);
+		
+		var cBasis = -GetParent<Node3D>().GlobalBasis.Y;
+		// var cBasis = -Basis.Y;
+		var axis = flatVel.Normalized().Cross(cBasis).Normalized();
+		
+		if (axis == Vector3.Zero) return;
+		
+		float goalRot = flatVel.Length() * _tiltDegPerMPerS;
+		float rotSpeed = 5;
+		if (flatVel.Length() < 0.1) 
+		{ goalRot = 0; rotSpeed = 15; }
+		else if (_player.Velocity.Y < -flatVel.Length()) 
+		{ goalRot = 0; rotSpeed = 5; }
+
+		var savedRot = Rotation;
+		Rotation = Vector3.Zero;
+		GlobalRotate(axis, Mathf.DegToRad(Mathf.Min(_tiltMaxDeg, goalRot)));
+		Rotation = savedRot.Lerp(Rotation, 1 - Mathf.Exp((float)-delta * rotSpeed));
+	}
+	
+	private void ViewBob(double delta)
+	{
 		float xyLength = _player.Velocity.WithY(0).Length();
+		
 		if (xyLength < 0.02 || !_player.IsGrounded)
 		{
 			_offset = 0;
@@ -24,18 +56,13 @@ public partial class Swayer : Node3D
 			return;
 		}
 		
-		_offset += (float)delta * _frequency * xyLength;
+		_offset += (float)delta * _stepFrequency * xyLength;
 		_offset %= Mathf.Tau;
-		Position = Vector3.Up * Mathf.Sin(_offset) * _amplitude * Mathf.Min(1, xyLength);
+		Position = Vector3.Up * Mathf.Sin(_offset) * _stepAmplitude * Mathf.Min(1, xyLength);
 		
 		bool check = (_offset / MathF.Tau + 0.75f) % 1 >= 0.5;
 		if (check && !_oldCheck)
 			EmitSignal(SignalName.DownStep);
 		_oldCheck = check;
-	}
-
-	private void ResetOffset()
-	{
-		// Offset = 0;
 	}
 }
