13b74156adf8b2a12d45f5897bd3406e509b820a
Fripe070 <72686066+Fripe070@users.noreply.github.com>
2024-08-21 00:21:02 +0200

    Show physics state in WASD display

--- scripts/gui/WasdHudControls.cs
+++ scripts/gui/WasdHudControls.cs
@@ -1,8 +1,16 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public partial class WasdHudControls : VBoxContainer
 {
+	[Export] private Color _pressedColor = Colors.Cyan;
+	[Export] private Color _releasedColor = Colors.DimGray;
+
+	[Export] private Color _physicsPressedColor = Colors.Cyan.Lerp(Colors.DimGray, 0.85f);
+	[Export] private Color _physicsReleasedColor = Colors.Black;
+	
+	[ExportGroup("Keys")]
 	[Export] private Label _wLabel;
 	[Export] private Label _aLabel;
 	[Export] private Label _sLabel;
@@ -12,31 +20,55 @@ public partial class WasdHudControls : VBoxContainer
 	[Export] private Label _leftClickLabel;
 	[Export] private Label _rightClickLabel;
 	
+
+	private Label[] _labels;
+	private Func<bool>[] _labelStates;
+	
 	public override void _Ready()
 	{
-		_wLabel.LabelSettings = (LabelSettings)_wLabel.LabelSettings.Duplicate();
-		_aLabel.LabelSettings = (LabelSettings)_aLabel.LabelSettings.Duplicate();
-		_sLabel.LabelSettings = (LabelSettings)_sLabel.LabelSettings.Duplicate();
-		_dLabel.LabelSettings = (LabelSettings)_dLabel.LabelSettings.Duplicate();
-		_spaceLabel.LabelSettings = (LabelSettings)_spaceLabel.LabelSettings.Duplicate();
-		_leftClickLabel.LabelSettings = (LabelSettings)_leftClickLabel.LabelSettings.Duplicate();
-		_rightClickLabel.LabelSettings = (LabelSettings)_rightClickLabel.LabelSettings.Duplicate();
+		_labels = new Label[7];
+		_labelStates = new Func<bool>[_labels.Length];
+		
+		_labels[0] = _wLabel;
+		_labelStates[0] = () => Input.IsActionPressed("move_forward");
+		_labels[1] = _aLabel;
+		_labelStates[1] = () => Input.IsActionPressed("move_left");
+		_labels[2] = _sLabel;
+		_labelStates[2] = () => Input.IsActionPressed("move_backward");
+		_labels[3] = _dLabel;
+		_labelStates[3] = () => Input.IsActionPressed("move_right");
+		_labels[4] = _spaceLabel;
+		_labelStates[4] = () => Input.IsActionPressed("jump");
+		_labels[5] = _leftClickLabel;
+		_labelStates[5] = () => Input.IsMouseButtonPressed(MouseButton.Left);
+		_labels[6] = _rightClickLabel;
+		_labelStates[6] = () => Input.IsMouseButtonPressed(MouseButton.Right);
+		
+		foreach (var label in _labels)
+		{
+			label.LabelSettings = (LabelSettings)label.LabelSettings.Duplicate();
+		}
 	}
 
 	public override void _Process(double delta)
 	{
-		Handle(_wLabel, Input.IsActionPressed("move_forward"));
-		Handle(_aLabel, Input.IsActionPressed("move_left"));
-		Handle(_sLabel, Input.IsActionPressed("move_backward"));
-		Handle(_dLabel, Input.IsActionPressed("move_right"));
-		Handle(_spaceLabel, Input.IsActionPressed("jump"));
-		
-		Handle(_leftClickLabel, Input.IsMouseButtonPressed(MouseButton.Left));
-		Handle(_rightClickLabel, Input.IsMouseButtonPressed(MouseButton.Right));
+		GeneralProcess();
 	}
 
-	private void Handle(Label label, bool state)
+	public override void _PhysicsProcess(double delta)
 	{
-		label.LabelSettings.FontColor = state ? Colors.Aqua : Colors.DarkGray;
+		GeneralProcess();
+	}
+
+	private void GeneralProcess()
+	{
+		for (var i = 0; i < _labels.Length; i++)
+		{
+			bool state = _labelStates[i]();
+			var label = _labels[i];
+			label.LabelSettings.FontColor = state ? _pressedColor : _releasedColor;
+			if (Engine.IsInPhysicsFrame())
+				label.GetParent<ColorRect>().Color = state ? _physicsPressedColor : _physicsReleasedColor;
+		}
 	}
 }
